486437a07489a7b7bf396636942cb249be4f4d48
Elin <elinnet@gmail.com>
2015-09-23 17:32:29 +0100

    Pre-Ptolemy intervention

--- lib/team.rb
+++ lib/team.rb
@@ -1,15 +1,27 @@
 class Team
-
+  attr_reader :team
   def initialize
     @team = []
   end
 
   def add_player player
-    @team << player
+    team << player
   end
 
   def valid?
-    @team.count > 6 && @team.count < 11
+    has_correct_number? && has_correct_gender_ratio?
+  end
+
+  private
+
+  def has_correct_number?
+    team.count > 6 && team.count < 11
+  end
+
+  def has_correct_gender_ratio?
+    all_genders = team.map { |player| player.gender}
+    gender_numbers = Hash[all_genders.group_by {|gender| gender}.map {|k,v| [k,v.count]}].values
+    gender_numbers.all? {|each_gender| each_gender > 1 }
   end
 
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,19 +1,37 @@
 require 'team'
 
 describe Team do
-  it 'is not valid if has less than 7 players' do
-    6.times { subject.add_player :player }
-    expect(subject).not_to be_valid
+
+  context 'validate numbers' do
+
+    it 'is not valid if has less than 7 players' do
+      6.times { subject.add_player :player }
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid if has more than 10 players' do
+      11.times { subject.add_player :player }
+      expect(subject).not_to be_valid
+    end
+
+    it 'is valid if has 8 players' do
+      8.times { subject.add_player :player }
+      expect(subject).to be_valid
+    end
   end
 
-  it 'is not valid if has more than 10 players' do
-    11.times { subject.add_player :player  }
-    expect(subject).not_to be_valid
+  context 'validate genders' do
+    let(:male_player)   {double :male_player, gender: :male}
+    let(:female_player) {double :female_player, gender: :female}
+
+    it 'is minimum of any one gender' do
+      1.times { subject.add_player male_player}
+      9.times { subject.add_player female_player}
+    
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'is valid if has 8 players' do
-    8.times { subject.add_player :player }
-    expect(subject).to be_valid
-  end
+
 
 end
